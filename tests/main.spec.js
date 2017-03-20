12bd9cbe2319a4c3ab4e9a721c3acb388a8600e7
Willian Justen <willianjustenqui@gmail.com>
2017-03-20 11:54:30 +0100

    Explaining bail, skip and only

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,18 +1,23 @@
+// --bail no console para parar no primeiro fail
+
 describe('Main', function() {
   describe('Method A', function() {
     context('Case 1', function() {
-      it('should happen blabla', function() {
+      it.skip('should happen blabla', function() {
         // espera que aconteça
         // Entra de dados / método sum(2,2)
         // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
       });
     });
 
     context('Case 2', function() {
+      // only para rodar só um bloco
       it('should happen blabla', function() {
         // espera que aconteça
         // Entra de dados / método sum(2,2)
         // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
       });
 
       it('should happen mimimi', function() {
