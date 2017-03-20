6e9e495a5eca32e33a28e4060245a0d107ebba42
Willian Justen <willianjustenqui@gmail.com>
2017-03-20 13:23:08 +0100

    Add hooks explanation

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,34 +1,40 @@
-// --bail no console para parar no primeiro fail
-
 describe('Main', function() {
-  describe('Method A', function() {
-    context('Case 1', function() {
-      it.skip('should happen blabla', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-        throw new Error('just an error');
-      });
-    });
 
-    context('Case 2', function() {
-      // only para rodar só um bloco
-      it('should happen blabla', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-        throw new Error('just an error');
-      });
-
-      it('should happen mimimi', function() {
-        // espera que aconteça
-        // Entra de dados / método sum(2,2)
-        // Espera retornar (4) => true | (3) => false => broken test
-      });
-    });
+  // roda uma vez, antes do bloco
+  before(function() {
+    console.log('before');
   });
 
-  describe('Method B', function() {
-
+  // roda uma vez, depois do bloco
+  after(function() {
+    console.log('after');
   });
+
+  // roda todas as vezes, antes de CADA bloco
+  beforeEach(function() {
+    console.log('beforeEach');
+  });
+
+  // roda todas as vezes, depois de CADA bloco
+  afterEach(function() {
+    console.log('afterEach');
+  });
+
+  it('test 1', function() {
+    console.log('test 1');
+  });
+
+  it('test 2', function() {
+    console.log('test 2');
+  });
+
 });
+
+// before
+// beforeEach
+// test 1
+// afterEach
+// beforeEach
+// test 2
+// afterEach
+// after
