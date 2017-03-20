3331034fa8d146799a3fabc6dadd4f3a81c3d448
Willian Justen <willianjustenqui@gmail.com>
2017-03-20 11:14:06 +0100

    Add tests explaining mocha blocks

---
+++ tests/main.spec.js
@@ -0,0 +1,29 @@
+describe('Main', function() {
+  describe('Method A', function() {
+    context('Case 1', function() {
+      it('should happen blabla', function() {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', function() {
+      it('should happen blabla', function() {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+
+      it('should happen mimimi', function() {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+  });
+
+  describe('Method B', function() {
+
+  });
+});
