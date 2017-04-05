36dc6e5773e01a74268108f942ee9bda631a1ed6
Willian Justen <willianjustenqui@gmail.com>
2017-04-06 01:24:32 +0200

    Converting to es6

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
-module.exports = {
-  sum: function(num1, num2) {
-    return num1 + num2;
-  },
-  sub: function(num1, num2) {
-    return num1 - num2;
-  },
-  mult: function(num1, num2) {
-    return num1 * num2;
-  },
-  div: function(num1, num2) {
-    return (num2 === 0) ? 'Não é possível divisão por zero!' : num1 / num2;
-  }
-}
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => (num2 === 0) ? 'Não é possível divisão por zero!' : num1 / num2;
+
+export { sum, sub, mult, div }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,66 +1,62 @@
-var expect = require('chai').expect;
-var calc = require('../src/main.js');
+import { expect } from 'chai';
+import { sum, sub, mult, div } from '../src/main';
 
-describe('Calc', function() {
+describe('Calc', () => {
 
   // smoke tests
-  describe('Smoke tests', function() {
+  describe('Smoke tests', () => {
 
-    it('should exist the calc lib', function() {
-      expect(calc).to.exist;
+    it('should exist the method `sum`', () => {
+      expect(sum).to.exist;
+      expect(sum).to.be.a.function;
     });
 
-    it('should exist the method `sum`', function() {
-      expect(calc.sum).to.exist;
-      expect(calc.sum).to.be.a.function;
+    it('should exist the method `sub`', () => {
+      expect(sub).to.exist;
+      expect(sub).to.be.a.function;
     });
 
-    it('should exist the method `sub`', function() {
-      expect(calc.sub).to.exist;
-      expect(calc.sub).to.be.a.function;
+    it('should exist the method `mult`', () => {
+      expect(mult).to.exist;
+      expect(mult).to.be.a.function;
     });
 
-    it('should exist the method `mult`', function() {
-      expect(calc.mult).to.exist;
-      expect(calc.mult).to.be.a.function;
-    });
-
-    it('should exist the method `div`', function() {
-      expect(calc.div).to.exist;
-      expect(calc.div).to.be.a.function;
+    it('should exist the method `div`', () => {
+      expect(div).to.exist;
+      expect(div).to.be.a.function;
     });
 
   });
 
-  describe('Sum', function() {
-    it('should return 4 when `sum(2,2)`', function() {
-      expect(calc.sum(2, 2)).to.be.equal(4);
+  describe('Sum', () => {
+    it('should return 4 when `sum(2,2)`', () => {
+      expect(sum(2, 2)).to.be.equal(4);
     });
   });
 
-  describe('Sub', function() {
-    it('should return 4 when `sub(6,2)`', function() {
-      expect(calc.sub(6, 2)).to.be.equal(4);
+  describe('Sub', () => {
+    it('should return 4 when `sub(6,2)`', () => {
+      expect(sub(6, 2)).to.be.equal(4);
     });
 
-    it('should return -4 when `sub(6,10)`', function() {
-      expect(calc.sub(6, 10)).to.be.equal(-4);
-    });
-  });
-
-  describe('Mult', function() {
-    it('should return 4 when `mult(2,2)`', function() {
-      expect(calc.mult(2, 2)).to.be.equal(4);
+    it('should return -4 when `sub(6,10)`', () => {
+      expect(sub(6, 10)).to.be.equal(-4);
     });
   });
 
-  describe('Div', function() {
-    it('should return 2 when `div(4,2)`', function() {
-      expect(calc.div(4, 2)).to.be.equal(2);
+  describe('Mult', () => {
+    it('should return 4 when `mult(2,2)`', () => {
+      expect(mult(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 2 when `div(4,2)`', () => {
+      expect(div(4, 2)).to.be.equal(2);
     });
 
-    it('should return `Não é possível divisão por zero!` when divide by 0', function() {
-      expect(calc.div(4, 0)).to.be.equal('Não é possível divisão por zero!');
+    it('should return `Não é possível divisão por zero!` when divide by 0', () => {
+      expect(div(4, 0)).to.be.equal('Não é possível divisão por zero!');
     });
   });
 });
